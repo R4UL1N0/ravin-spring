e2e0a2ba96d66a611f7efe3bfd6a02d6709eda0b
R4UL1N0 <raulino.storage@gmail.com>
2023-08-28 20:28:43 -0300

    lombok nos dtos e estudo de @Controller estilo view

--- src/main/java/br/com/devxlabs/ravin/models/dtos/PersonDTO.java
+++ src/main/java/br/com/devxlabs/ravin/models/dtos/PersonDTO.java
@@ -3,6 +3,8 @@ package br.com.devxlabs.ravin.models.dtos;
 import java.io.Serializable;
 import java.util.Date;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -13,10 +15,15 @@ public class PersonDTO implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
 	
+	@NotBlank @NotNull
 	private String name;
+	@NotBlank @NotNull
 	private AddressDTO address;
+	@NotBlank @NotNull
 	private String phone;
+	@NotBlank @NotNull
 	private String cpf;
+	@NotBlank @NotNull
 	private Date dateOfBirth;
 	private String observations;
 	private boolean hasActive;
--- src/main/java/br/com/devxlabs/ravin/models/entities/Address.java
+++ src/main/java/br/com/devxlabs/ravin/models/entities/Address.java
@@ -1,6 +1,8 @@
 package br.com.devxlabs.ravin.models.entities;
 
 import jakarta.persistence.Embeddable;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -10,9 +12,13 @@ import lombok.ToString;
 @Getter @Setter @NoArgsConstructor @ToString
 public class Address {
 
+	@NotBlank @NotNull
 	private String state;
+	@NotBlank @NotNull
 	private String city;
+	@NotBlank @NotNull
 	private String street;
+	@NotBlank @NotNull
 	private String zipCode;
 
 }
